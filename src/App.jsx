f88ad52b21831a8f5dd7b61817f87694ac892e70
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-05-14 17:53:07 +0530

    reusbility and fragment

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,9 @@ function App() {
   const data = ["aman", "deep", "singh", "parihar"];
 
   return (
-    <div>
-      {data.map(() => {
-        return <h1>hey</h1>;
-      })}
-    </div>
+    <>
+      <Card/>
+    </>
   );
 }
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,47 @@
 import React from "react";
 
 function Card() {
+  const data = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1624521793559-136bfe16fc86?q=80&w=2000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Amazon Basics",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aliquam ",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Daily Objects",
+      description:
+        "headphones ipsum dolor sit amet consectetur adipisicing elit. Totam aliquam ",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1533749047139-189de3cf06d3?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Clock",
+      description:
+        "clock ipsum dolor sit amet consectetur adipisicing elit. Totam aliquam ",
+    },
+  ];
+
   return (
-    <div className="w-full h-screen bg-zinc-200">
-      <div className="w-52 bg-zinc-100 absolute left-1/2 top-1/2 -translate-x-[50%] -translate-y-[50%] rounded-md overflow-hidden">
-        <div className="w-full h-32 bg-zinc-300">
-            <img className="w-full h-full object-cover" src="https://images.unsplash.com/photo-1633174524827-db00a6b7bc74?q=80&w=1796&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+    <div className="w-full h-screen flex items-center justify-center gap-10 bg-zinc-200">
+      {data.map((elem, index) => (
+        <div key={index} className="w-52 bg-zinc-100 rounded-md overflow-hidden">
+          <div className="w-full h-32 bg-zinc-300">
+            <img
+              className="w-full h-full object-cover"
+              src={elem.image}
+              alt=""
+            />
+          </div>
+          <div className="w-full px-3 py-4">
+            <h2 className="font-semibold">{elem.name}</h2>
+            <p className="text-xs mt-5">{elem.description}</p>
+          </div>
         </div>
-        <div className="w-full px-3 py-4">
-            <h2 className="font-semibold">Amazon Basics</h2>
-            <p className="text-xs mt-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam aliquam </p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 }
