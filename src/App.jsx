c58672bdd4ba8689caaa6ac6f7561d1b4cc699ce
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-05-30 19:11:55 +0530

    learning of use case on going

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 // import Card from "./Components/Card";
 import Card2 from "./Components/Card2";
+import "./App.css";
 
 function App() {
   // const data = ["aman", "deep", "singh", "parihar"];
+  const [val, setVal] = useState({ name: "Amandeep", isBanned: false });
 
   return (
-    <>
+    <div>
       {/* <Card></Card> */}
-      <Card2></Card2>
-    </>
+      {/* <Card2></Card2> */}
+      <h1>Name : {val.name}</h1>
+      <h1>Banned : {val.isBanned.toString()}</h1>
+      <button
+        className={`${
+          val.isBanned ? "btn blue" : "btn red"}`}
+        onClick={() => setVal({ ...val, isBanned: !val.isBanned })}>
+        Click
+      </button>
+    </div>
   );
 }
 
 export default App;
+ 
