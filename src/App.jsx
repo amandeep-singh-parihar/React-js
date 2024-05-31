19632f370c3307bfca5bbecf6532f2abacd73fce
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-05-31 15:34:03 +0530

    updating object array

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,33 @@ import "./App.css";
 function App() {
   // const data = ["aman", "deep", "singh", "parihar"];
   // const [val, setVal] = useState({ name: "Amandeep", age: 20 });
-  const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
+  // const [val, setVal] = useState([1, 2, 3, 4, 5, 6]);
+  const [val, setVal] = useState([
+    { name: "Amandeep", age: 20 },
+    { name: "Abhay", age: 21 },
+    { name: "Nitin", age: 22 },
+  ]);
 
   return (
     <div>
       {/* <Card></Card> */}
       {/* <Card2></Card2> */}
-      {val.map(item=><h1>{item}</h1>)}
-      <button onClick={()=>setVal([...val,val[val.length-1]+1])} className="btn blue">click</button>
+      {val.map((item) => (
+        <div>
+          <h1>{item.name}</h1>
+          <h1>{item.age}</h1>
+        </div>
+      ))}
+      <button
+        onClick={() =>
+          setVal(() =>
+            val.map((item) =>
+              item.name === "Abhay" ? { name: "Abhay", age: 100 } : item
+            )
+          )
+        }
+        className="btn blue"
+      > click </button>
     </div>
   );
 }
