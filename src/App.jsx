2f4d373770fe3722af995063074ca0a165af132b
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-06-02 01:53:19 +0530

    #card4 working i progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
-import Card from "./Components/Card";
-import Card2 from "./Components/Card2";
-import Card3 from "./Components/Card3";
+// import Card from "./Components/Card";
+// import Card2 from "./Components/Card2";
+// import Card3 from "./Components/Card3";
 import Card4 from "./Components/Card4";
 import "./App.css";
 
 function App() {
   return (
     <div>
-      <Card></Card>
+      <Card4></Card4>
     </div>
   );
 }
--- src/Components/Card4.jsx
+++ src/Components/Card4.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { FaArrowRightLong } from "react-icons/fa6";
 import "./Card4.css";
 
 function Card4() {
   return (
     <div className="container">
       <div className="card">
-        <img src="https://plus.unsplash.com/premium_photo-1675315343167-0414307c9182?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <img
+          src="https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt=""
+        />
+        <span className="arrow">
+          <FaArrowRightLong size={"0.8em"} />
+        </span>
       </div>
     </div>
   );
