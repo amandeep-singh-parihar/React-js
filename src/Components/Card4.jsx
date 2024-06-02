37c3d6518879db04704869cd898142f21d798f52
amandeep-singh-parihar <amandeepparihar24@gmail.com>
2024-06-02 15:31:03 +0530

    adding the arrow invertion effect

--- src/Components/Card4.jsx
+++ src/Components/Card4.jsx
@@ -18,7 +18,7 @@ function Card4() {
           src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           alt=""
         />
-        <span onClick={() => setVal(() => !val)} className="arrow">
+        <span onClick={() => setVal(() => !val)} className={`arrow ${val ? "rotate" : ""}`}>
           <FaArrowRightLong size={"0.8em"} />
         </span>
       </div>
